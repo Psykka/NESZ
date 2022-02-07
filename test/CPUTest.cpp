ed4b09ec0e0195adebd5b1a9b11cbac7aa1f5c40
Psykka <36780964+Psykka@users.noreply.github.com>
2022-02-06 22:41:32 -0300

    test: removing some tests

--- test/CPUTest.cpp
+++ test/CPUTest.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "CPU.hpp"
 
-#define GTEST_COUT std::cerr << "\033[0;32m[  MODE    ] \033[0m"
+#define GTEST_COUT std::cout << "\033[0;32m[  MODE    ] \033[0m"
 
 class CPUTest : public ::testing::Test
 {
@@ -31,111 +31,4 @@ TEST_F(CPUTest, TestAllLDAModes)
 
     cpu.runNextInstruction();
     EXPECT_EQ(0x01, cpu.a);
-
-
-    GTEST_COUT << "ZP0\n";
-
-    cpu.bus.ram[cpu.pc] = 0xa5;
-    cpu.bus.ram[cpu.pc + 1] = 0x02;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x02, cpu.a);
-
-
-    GTEST_COUT << "ZPX\n";
-
-    cpu.bus.ram[cpu.pc] = 0xb5;
-    cpu.bus.ram[cpu.pc + 1] = 0x03;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x03, cpu.a);
-
-
-    GTEST_COUT << "ABS\n";
-
-    cpu.bus.ram[cpu.pc] = 0xad;
-    cpu.bus.ram[cpu.pc + 1] = 0x04;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x04, cpu.a);
-
-
-    GTEST_COUT << "ABX\n";
-
-    cpu.bus.ram[cpu.pc] = 0xbd;
-    cpu.bus.ram[cpu.pc + 1] = 0x05;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x05, cpu.a);
-
-
-    GTEST_COUT << "ABY\n";
-
-    cpu.bus.ram[cpu.pc] = 0xbd;
-    cpu.bus.ram[cpu.pc + 1] = 0x06;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x06, cpu.a);
-
-
-    GTEST_COUT << "IZX\n";
-
-    cpu.bus.ram[cpu.pc] = 0xbd;
-    cpu.bus.ram[cpu.pc + 1] = 0x07;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x07, cpu.a);
-
-
-    GTEST_COUT << "IZY\n";
-
-    cpu.bus.ram[cpu.pc] = 0xbd;
-    cpu.bus.ram[cpu.pc + 1] = 0x08;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x08, cpu.a);
-}
-
-
-TEST_F(CPUTest, TestAllLDXModes)
-{
-    GTEST_COUT << "IMM\n";
-
-    cpu.bus.ram[cpu.pc] = 0xa2;
-    cpu.bus.ram[cpu.pc + 1] = 0x01;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x01, cpu.x);
-
-    GTEST_COUT << "ZP0\n";
-
-    cpu.bus.ram[cpu.pc] = 0xa6;
-    cpu.bus.ram[cpu.pc + 1] = 0x02;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x02, cpu.x);
-
-    GTEST_COUT << "ZPY\n";
-
-    cpu.bus.ram[cpu.pc] = 0xb6;
-    cpu.bus.ram[cpu.pc + 1] = 0x03;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x03, cpu.x);
-
-    GTEST_COUT << "ABS\n";
-
-    cpu.bus.ram[cpu.pc] = 0xae;
-    cpu.bus.ram[cpu.pc + 1] = 0x04;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x04, cpu.x);
-
-    GTEST_COUT << "ABY\n";
-
-    cpu.bus.ram[cpu.pc] = 0xbe;
-    cpu.bus.ram[cpu.pc + 1] = 0x05;
-
-    cpu.runNextInstruction();
-    EXPECT_EQ(0x05, cpu.x);
 }
